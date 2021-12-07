cd15d82d4956853d28a27cd6263fb6f98738f8f0
elahe mottaghin <elahemotaghin@gmail.com>
2021-12-07 13:37:10 +0330

    add some new Apis

--- DockerProject/dockerProject/RestAPI/urls.py
+++ DockerProject/dockerProject/RestAPI/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import gameInformation, bestGamesByPlatform
+from .views import gameInformation, searchByName, bestGamesByPlatform, bestGamesByYear, bestGamesByGenre, best5GamesBySale, compareBySale
 
 urlpatterns = [
     path('gameInformation/', gameInformation, name='gameInformation'),#http://127.0.0.1:8000/RestAPI/gameInformation/
+    path('searchByName/', searchByName, name='searchByName'),  # http://127.0.0.1:8000/RestAPI/searchByName/
     path('bestGamesByPlatform/', bestGamesByPlatform, name='bestGamesByPlatform'),#http://127.0.0.1:8000/RestAPI/bestGamesByPlatform/
+    path('bestGamesByYear/', bestGamesByYear, name='bestGamesByYear'),#http://127.0.0.1:8000/RestAPI/bestGamesByYear/
+    path('bestGamesByGenre/', bestGamesByGenre, name='bestGamesByGenre'),  # http://127.0.0.1:8000/RestAPI/bestGamesByGenre/
+    path('best5GamesBySale/', best5GamesBySale, name='best5GamesBySale'),  # http://127.0.0.1:8000/RestAPI/best5GamesBySale/
+    path('compareBySale/', compareBySale, name='compareBySale'),  # http://127.0.0.1:8000/RestAPI/compareBySale/
+    path('searchByName/', searchByName, name='searchByName'),  # http://127.0.0.1:8000/RestAPI/searchByName/
 ]
--- DockerProject/dockerProject/RestAPI/views.py
+++ DockerProject/dockerProject/RestAPI/views.py
@@ -15,11 +15,62 @@ def gameInformation(request):
     return HttpResponse(data)
 
 @csrf_exempt
+def searchByName(request):
+    name = request.POST.get('name')
+    game = Game.objects.filter(name__contains=name)
+    game_serialized = serializers.serialize('json', game)
+    game_json = json.loads(game_serialized)
+    data = json.dumps(game_json)
+    return HttpResponse(data)
+
+@csrf_exempt
 def bestGamesByPlatform(request):
     n = request.POST.get('n')
     platform = request.POST.get('platform')
     allGames = Game.objects.filter(platform=platform).all()[:int(n)]
     game_serialized = serializers.serialize('json', allGames)
+    game_json = json.loads(game_serialized)
+    data = json.dumps(game_json)
+    return HttpResponse(data)
+
+@csrf_exempt
+def bestGamesByYear(request):
+    n = request.POST.get('n')
+    year = request.POST.get('year')
+    allGames = Game.objects.filter(year=year).all()[:int(n)]
+    game_serialized = serializers.serialize('json', allGames)
+    game_json = json.loads(game_serialized)
+    data = json.dumps(game_json)
+    return HttpResponse(data)
+
+@csrf_exempt
+def bestGamesByGenre(request):
+    n = request.POST.get('n')
+    genre = request.POST.get('genre')
+    allGames = Game.objects.filter(genre=genre).all()[:int(n)]
+    game_serialized = serializers.serialize('json', allGames)
+    game_json = json.loads(game_serialized)
+    data = json.dumps(game_json)
+    return HttpResponse(data)
+
+@csrf_exempt
+def best5GamesBySale(request):
+    year = request.POST.get('year')
+    platform = request.POST.get('platform')
+    allGames = Game.objects.filter(year=year , platform=platform).all().order_by('-Global_Sales')[:5]
+    game_serialized = serializers.serialize('json', allGames)
+    game_json = json.loads(game_serialized)
+    data = json.dumps(game_json)
+    return HttpResponse(data)
+
+@csrf_exempt
+def compareBySale(request):
+    allGames = Game.objects.all()
+    filterGames = []
+    for game in allGames:
+        if game.EU_Sales > game.NA_Sales:
+            filterGames.append(game)
+    game_serialized = serializers.serialize('json', filterGames)
     game_json = json.loads(game_serialized)
     data = json.dumps(game_json)
     return HttpResponse(data)
